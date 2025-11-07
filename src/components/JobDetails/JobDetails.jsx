39ca313ed4f9ad9e2386aea8f922d735316ff749
raworiginal <118763950+raworiginal@users.noreply.github.com>
2025-11-07 08:44:16 -0800

    created job detail

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,24 +1,43 @@
-import { useParams } from 'react-router';
-import { useState, useEffect } from 'react';
-import * as jobService from '../../services/jobService';
-
+import { useParams } from "react-router";
+import { useState, useEffect } from "react";
+import * as jobService from "../../services/jobService";
 
 const JobDetails = () => {
-    const { jobId } = useParams();
-    console.log('jobId', jobId);
+	const { jobId } = useParams();
+	console.log("jobId", jobId);
 
-    const [job, setJob] = useState(null);
+	const [job, setJob] = useState(null);
 
-    useEffect(() => {
-        const fetchJob = async () => {
-            const jobData = await jobService.show(jobId);
-            setJob(jobData);
-        };
-        fetchJob()
-    }, [jobId]);
-    console.log('job state:', job);
-
-    return <main>Job Details</main>;
+	useEffect(() => {
+		const fetchJob = async () => {
+			const jobData = await jobService.show(jobId);
+			setJob(jobData);
+		};
+		fetchJob();
+	}, [jobId]);
+	console.log("job state:", job);
+	if (!job) return <main>loading...</main>;
+	return (
+		<main className="container">
+			<article>
+				<header>
+					<h1>Job Title: {job.jobTitle}</h1>
+					<p>Company: {job.companyName.toUpperCase()}</p>
+					<p>Date Applied: {job.dateApplied}</p>
+				</header>
+				<p>Website: {job.companyWebsite}</p>
+				<p>LinkedIn: {job.companyLinkedIn}</p>
+				<p>Job Description: {job.jobDescription}</p>
+				<p>{job.referral ? `Referral: ${job.referral}` : "N/A"}</p>
+				<p>Status: {job.status}</p>
+				<p>{`notes: ${job.notes}` || ""}</p>
+			</article>
+			<footer>
+				<button>Edit</button>
+				<button>Delete</button>
+			</footer>
+		</main>
+	);
 };
 
 export default JobDetails;
